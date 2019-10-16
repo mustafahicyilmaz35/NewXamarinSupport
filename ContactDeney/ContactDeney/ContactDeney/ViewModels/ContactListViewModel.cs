81f6c33c2843f3c49d2a89899f44bf751eab9942
mustafahicyilmaz35 <mustafahicyilmaz35@gmail.com>
2019-10-16 21:09:41 +0300

    Initalize

--- ContactDeney/ContactDeney/ContactDeney/ViewModels/ContactListViewModel.cs
+++ ContactDeney/ContactDeney/ContactDeney/ViewModels/ContactListViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Net.Mime;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -15,11 +16,29 @@ namespace ContactDeney.ViewModels
 {
     class ContactListViewModel : INotifyPropertyChanged
     {
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ObservableCollection<ContactItem> ContactItems { get; private set; }
 
+        
+        
+
+        private string _searchedText;
+
+        public string SearchedText
+        {
+            get { return _searchedText; }
+            set
+            {
+                _searchedText = value;
+                OnPropertyChanged(nameof(SearchedText));
+            }
+        }
+
         public ICommand AddNewCommand { get; private set; }
+        public ICommand SearchCommand { get; private set; }
+
 
         public ContactListViewModel()
         {
@@ -31,6 +50,21 @@ namespace ContactDeney.ViewModels
             });
 
             AddNewCommand = new Command(AddNewAction);
+            SearchCommand = new Command(SearchCommandExecute);
+        }
+
+        private void SearchCommandExecute()
+        {
+            if (SearchedText != null)
+            {
+                var query = ContactItems.Where(c => c.FirstName.StartsWith(SearchedText));
+                foreach (var item in query)
+                {
+                    ContactItems.Add(item);                    
+                }
+            }
+
+            
         }
 
         void AddNewAction()
--- ContactDeney/ContactDeney/ContactDeney/Views/ContactListPage.xaml.cs
+++ ContactDeney/ContactDeney/ContactDeney/Views/ContactListPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ContactDeney.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,22 @@ namespace ContactDeney.Views
         public ContactListPage()
         {
             InitializeComponent();
+        }
+
+        private void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.NewTextValue))
+            {
+                lst_Contact.ItemsSource = (BindingContext as ContactListViewModel).ContactItems;
+                
+
+            }
+            else
+            {
+                lst_Contact.ItemsSource =
+                    (BindingContext as ContactListViewModel).ContactItems.Where(c =>
+                        c.FirstName.ToLower().StartsWith(e.NewTextValue.ToLower()));
+            }
         }
     }
 }
